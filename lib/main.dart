d02545814d3f453a98a0f67409d92631bd8ba47c
Vincenzo0426 <113822102+Vincenzo0426@users.noreply.github.com>
2022-11-11 19:57:18 +0800

    commit_1111

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
+  const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-
-    var appTitle = const Text('image_409631628');
-
-    var hiFlutter = const Text('🐸勒', style: TextStyle(fontSize: 60),);
-
-    var appBody = Center(child: hiFlutter,);
-
-    var appBar = AppBar(title: appTitle,);
-
-    var app = MaterialApp(
-        home: Scaffold(appBar: appBar, body: appBody,)
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: "flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const ImageBrowse(title: 'image_409631628'),
     );
-
-    return app;
   }
 }
+
+class ImageBrowse extends StatefulWidget
+{
+  const ImageBrowse({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<ImageBrowse> createState() => _ImageBrowseState();
+}
+
+class _ImageBrowseState extends State<ImageBrowse>
+{
+  var images=['assets/RRRRRRR.jpg', 'assets/disgusting.jpg', 'assets/ikun.jpg', 'assets/roger.jpg'];
+  int imageindex = 1;
+  String defult_image = "assets/RRRRRRR.jpg";
+
+  void down()
+  {
+    if(imageindex <= 0){imageindex=4;}
+    defult_image = images[imageindex-1];
+    imageindex = imageindex-1;
+  }
+  void up()
+  {
+    if(imageindex >= 3){imageindex=-1;}
+    defult_image = images[imageindex+1];
+    imageindex = imageindex+1;
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+      (
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap:(){
+                const snackBar = SnackBar(
+                  content: Text('                       🐸勒', textScaleFactor: 1.8,),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Container(
+                height: 300,
+                width: 400,
+                child: Image.asset(defult_image),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: (){
+                        setState(() {
+                          down();
+                        });
+                      },
+                      child: Text("上一張圖片",
+                        textScaleFactor:1.3,)
+                  ),
+                  SizedBox(width: 100,),
+                  ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        up();
+                      });
+                    },
+                    child: Text("下一張圖片",
+                      textScaleFactor:1.3,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
